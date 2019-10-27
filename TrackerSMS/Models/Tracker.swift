dc0259ca7d27b3f7c0d0ad67448b2b928242fd7c
Dennis Sørensen <dennissorensen24@hotmail.dk>
2019-10-27 18:00:51 +0100

    startop på trackerSMS

---
+++ TrackerSMS/Models/Tracker.swift
@@ -0,0 +1,13 @@
+//
+//  Tracker.swift
+//  TrackerSMS
+//
+//  Created by Dennis Sørensen on 27/10/2019.
+//  Copyright © 2019 Dennis Sørensen. All rights reserved.
+//
+
+import Foundation
+
+class Tracker {
+    
+}
