1312c0b3c862072d1b0f639f13c9b2b2648e6f00
Dennis Sørensen <dennissorensen24@hotmail.dk>
2019-10-27 17:43:57 +0100

    Initial Commit

---
+++ TrackerSMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackerSMS
+//
+//  Created by Dennis Sørensen on 27/10/2019.
+//  Copyright © 2019 Dennis Sørensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
